4ae3e89cdb5cf0266ed7956af21101e2f250b3d0
Hamdi <hamdi.rahal@gmail.com>
2021-03-19 13:01:04 +0100

    HiddenSearchWidget

--- src/containers/HiddenSearchWidget/HiddenSearchWidget.jsx
+++ src/containers/HiddenSearchWidget/HiddenSearchWidget.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+// const search = document.querySelector('.search')
+// const btn = document.querySelector('.btn')
+// const input = document.querySelector('.input')
 
+// btn.addEventListener('click', () => {
+//     search.classList.toggle('active')
+//     input.focus()
+// })
 const HiddenSearchWidget = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [active, setactive] = useState(false);
+  return (
+    <div className="hidden-search-widget">
+      <div className={`search ${active && "active"}`}>
+        <input
+          type="text"
+          className="input"
+          placeholder="Search..."
+          autoFocus
+        />
+        <button className="btn" onClick={() => setactive(!active)}>
+          <i
+            className="fas fa-search"
+            style={{ color: "#333", marginLeft: "-10px" }}
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default HiddenSearchWidget
+export default HiddenSearchWidget;
