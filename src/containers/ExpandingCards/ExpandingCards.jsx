75db8ce06a880c9cc74887a250b8db65360a2f38
Hamdi <hamdi.rahal@gmail.com>
2021-03-23 19:59:36 +0100

     

--- src/containers/ExpandingCards/ExpandingCards.jsx
+++ src/containers/ExpandingCards/ExpandingCards.jsx
@@ -1,17 +1,4 @@
 import { useState } from "react";
-// const panels = document.querySelectorAll(".panel");
-// console.log(panels);
-// panels.forEach((panel) => {
-//   panel.addEventListener("click", () => {
-//     removeActiveClasses();
-//     panel.classList.add("active");
-//   });
-// });
-// function removeActiveClasses() {
-//   panels.forEach((panel) => {
-//     panel.classList.remove("active");
-//   });
-// }
 
 const ExpandingCards = () => {
     const [classList, setclassList] = useState(1)
